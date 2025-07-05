4d9268b4ca006f0d911e630838cf166d2641691c
Caetands1 <caetands@yahoo.com>
2025-07-05 01:17:37 +0100

    style modulisation/fix

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Request
+from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse,RedirectResponse
 
+import psycopg3
 from pathlib import Path
 
 import json
@@ -10,6 +12,8 @@ from playwright.async_api import async_playwright
 
 app = FastAPI()
 templates = Jinja2Templates(directory="templates")
+
+app.mount("/styles", StaticFiles(directory="templates/styles"), name="styles")
 
 @app.get("/", response_class=HTMLResponse)
 async def home():
